31e19b476355314ed8a924a167c0c4e641319e0b
WisdomLLong <f_l_wang@163.com>
2018-01-08 17:53:33 +0800

    Update HDFS-shell-python.py

--- Hadoop-Spark/Demo/HDFS-shell-python.py
+++ Hadoop-Spark/Demo/HDFS-shell-python.py
@@ -18,4 +18,45 @@ from hdfs3 import HDFileSystem
 test_host = 'localhost'
 test_port = 9000
 
+def hdfs_exists(hdfs_client):
+  path = '/tmp/test'
+  if hdfs_client.exists(path):
+    hdfs_client.rm(path)
+    
+  hdfs_client.makedirs(path)
+  
+def hdfs_write_read(hdfs_client):
+  data = b"hello hadoop" * 20   # b表示转化为二进制形式的
+  file a = 'tmp/test/file_a'
+  with hdfs_client.open(file_a, 'wb', replication=1) as f:
+    f.write(data)
+    
+  with hdfs_client.open(file_a, 'rb') as f:
+    out = f.read(len(data))   # read()里面的参数表示读取多少个字节
+    
+    assert out == data    # 断言，如果不相同，抛出错误
+
+def hdfs_readlines(hdfs_client):
+  file a = 'tmp/test/file_b'
+  with hdfs_client.open(file_b, 'wb', replication=1) as f:
+    f.write(b"hello\nhadoop")
+    
+  with hdfs_client.open(file_b, 'rb') as f:
+    lines = f.readlines()   
+    
+  assert len(lines)==2
+    
 if __name__ == '__main__':
+  hdfs_client = HDFilesSystem(host=test_host, port=test_port)
+  
+  hdfs_exists(hdfs_client)
+  
+  hdfs_write_read(hdfs_client)
+  
+  hdfs_readlines(hdfs_client)
+  
+  hdfs_client.disconnect()
+  
+  print("-" * 20)
+  print("hello hadoop")
+  
